ed9f97d603dd4cd76527eda62ab325974afc7ccd
Scott Waite <scomttwaite@gmail.com>
2013-09-18 20:39:36 -0400

    FreshRepo

---
+++ Week1/iOS/www/main.js
@@ -0,0 +1,127 @@
+
+
+<script>
+function onSuccess(heading) {
+    alert('Heading: ' + heading.magneticHeading);
+};
+
+function onError(error) {
+    alert('CompassError: ' + error.code);
+};
+
+navigator.compass.getCurrentHeading(onSuccess, onError);
+
+</script>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+    function init() {
+      document.addEventListener("deviceready", onDeviceReady, false);
+    }
+
+    function onDeviceReady() {
+      alert('Welcome to our StreetLinks app!');
+    }
+  </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+ function checkconnection() {
+        checkConnection();
+    }
+
+    function checkConnection() {
+        var networkState = navigator.network.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        alert('Your device has ' + states[networkState]);
+    }
+
+</script>
---
+++ Week2/iOS/www/main copy.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+
+
+
+
+function DisplayInstaForm()
+{
+	console.log('instagram');
+	
+	$('#instagram content div:last').html('https://api.instagram.com/v1/users/376080995/media/recent?access_token=376080995.5b9e1e6.e3cdf1450ae345308e32fb59dc1d6150');
+}
+
+
+
+
+
+
+});
+
+
+
