750044df7523ad4919140564f13df28f3298e626
Scott Waite <scomttwaite@gmail.com>
2013-09-25 19:10:07 -0400

    Updated Somes Files

--- Week3/iOS/www/main.js
+++ Week3/iOS/www/main.js
@@ -50,3 +50,44 @@ $('#Instagram').on('click', function () {
 
 Notification, Splashscreen, Connection, Geolocation
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Instagram
+
+
+$.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'jsonp'
+    success: function(received){
+        console.log(received);
+        $.each(received.data, function(index, photo){
+            var stRes = photo.user.id,
+            user_id = photo.user.id,
+            fullName = photo.user.full_name,
+            likes = photo.likes,
+            pic = "<li><img src='" + stRes + "' alt='" + user_id + "' /><h3>
+created by: " + fullName + ", Likes: " + likes + "<h3></li>";
+
+                    $('#results').append(pic);
+        });
+    }
+})
+
+
+
+
+
+
+
